60487d29ec33502f8c6285452bc1baf27d95d6e4
Kapil-Bhardwaj <kapisbhardwaj41@gmail.com>
2024-03-29 12:29:11 +0530

    completed basic layout of app

---
+++ lib/screens/meals_details.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:meals/Models/meal.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class MealsDetailScreen extends StatelessWidget {
+  const MealsDetailScreen({super.key, required this.meal});
+  final Meal meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            meal.title,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+                height: 300,
+                width: double.infinity,
+              ),
+              
+              const SizedBox(
+                height: 14,
+              ),
+              Text(
+                "Ingredients",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              for (final ingridients in meal.ingredients)
+                Text(
+                  ingridients,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(
+                "Steps",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              for (final stepText in meal.steps)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  child: Text(
+                    stepText,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ),
+              const SizedBox(
+                height: 4,
+              ),
+            ],
+          ),
+        ));
+  }
+}
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:meals/Models/meal.dart';
+import 'package:meals/screens/meals_details.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:meals/screens/meals_details.dart';
 
+import '../screens/meals.dart';
 class MealItem extends StatelessWidget {
   const MealItem({super.key, required this.meal});
   final Meal meal;
+  String get complexity {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordability {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
+
+  void _onSelectMeal(BuildContext context, String title){
+    // Navigator.of(context).push(MaterialPageRoute(
+    //     builder: (ctx)=> MealsDetailScreen(meal: meal)
+    //
+    // ));
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (ctx) => MealsDetailScreen(meal: meal),
+        ));}
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          _onSelectMeal(context,meal.title);
+          },
         child: Stack(
           children: [
             FadeInImage(
-                placeholder: MemoryImage(kTransparentImage),
-                image: NetworkImage(meal.imageUrl)),
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
             Positioned(
                 bottom: 0,
                 left: 0,
@@ -39,8 +70,22 @@ class MealItem extends StatelessWidget {
                       const SizedBox(
                         height: 12,
                       ),
-                     const  Row(
-                        children: [],
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          MealItemTrait(
+                              label: "${meal.duration.toString()} min",
+                              icon: Icons.schedule),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          MealItemTrait(label: complexity, icon: Icons.work),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          MealItemTrait(
+                              label: affordability, icon: Icons.attach_money),
+                        ],
                       )
                     ],
                   ),
